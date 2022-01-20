fd1ae7ed9717abf4c517abb83fada8b3da56f495
Matthew Schlegel <mkschleg@gmail.com>
2022-01-20 16:05:03 -0700

    Update to keep track of heads.

--- perf/FluxRNNsBench.jl
+++ perf/FluxRNNsBench.jl
@@ -8,10 +8,20 @@ using Statistics
 using ProgressMeter
 using DataFrames
 using CSV
+using Git
 
 
 
 include("bench_utils.jl")
+
+function git_head()
+    try
+        s = read(`$(Git.git()) rev-parse HEAD`, String)
+        s[1:end-1]
+    catch
+        "0"
+    end
+end
 
 # rng = Random.MersenneTwister(10)
 
@@ -71,12 +81,12 @@ function get_runtimes_3d(cell; Ns = [2, 20, 200], Ts = [1, 8, 16, 64])
 
 
     Random.seed!(10)
-    @showprogress 1 "MatrixSize: " for n in Ns
-        @showprogress 1 "TemporalLength: " 1 for t in Ts
+    @showprogress "MatrixSize: " for n in Ns
+        @showprogress "TemporalLength: " 1 for t in Ts
             x = randn(Float32, n, n, t)
             rnn_model = cell(n, n)
 
-            timings = run_benchmark_recur_3d(rnn_model, x, cuda=false)
+            timings = run_benchmark_recur(rnn_model, x, cuda=false)
             for k in keys(timings)
                 push!(RUNTIMES[k], ("$(cell)", false, n, t, get_statistics(timings[k])...))
             end
@@ -90,10 +100,10 @@ function get_runtimes_3d(cell; Ns = [2, 20, 200], Ts = [1, 8, 16, 64])
     RUNTIMES
 end
 
-function run_benchmark(cells=[RNN, GRU, LSTM]; save_dir="results", kwargs...)
+function run_benchmark(cells=[RNN, GRU, LSTM]; save_dir="results/$(git_head())", kwargs...)
 
     if !isdir(save_dir)
-        mkdir(save_dir)
+        mkpath(save_dir)
     end
 
     for cell in cells
@@ -105,6 +115,12 @@ function run_benchmark(cells=[RNN, GRU, LSTM]; save_dir="results", kwargs...)
         rt = get_runtimes(cell; kwargs...)
         for k in keys(rt)
             filename = joinpath(cell_svpath, "$(k).csv")
+            CSV.write(filename, rt[k])
+        end
+
+        rt = get_runtimes_3d(cell; kwargs...)
+        for k in keys(rt)
+            filename = joinpath(cell_svpath, "$(k)_3d.csv")
             CSV.write(filename, rt[k])
         end
     end
--- perf/bench_utils.jl
+++ perf/bench_utils.jl
@@ -9,50 +9,11 @@ bw(back) = back(1f0)
 fwbw(m, ps, x) = gradient(() -> sum(m(x)), ps)
 
 
-  
-function run_benchmark(model, x; cuda=true)
-    
-    if cuda 
-        model = model |> gpu
-        x = x |> gpu
-    end
-
-    ps = Flux.params(model)
-    y, back = pullback(() -> sum(model(x)), ps)
-
-
-    if cuda
-        CUDA.allowscalar(false)
-        # CUDA.device!(3)
-        println("  forward")
-        fw(model, x); GC.gc(); CUDA.reclaim(); #warmup
-        @btime CUDA.@sync(fw($model, $x)) teardown=(GC.gc(); CUDA.reclaim())
-
-        println("  backward")
-        bw(back); GC.gc(); CUDA.reclaim(); #warmup
-        @btime CUDA.@sync(bw($back)) teardown=(GC.gc(); CUDA.reclaim())
-        
-        println("  forw and back")
-        fwbw(model, ps, x); GC.gc(); CUDA.reclaim(); #warmup
-        @btime CUDA.@sync(fwbw($model, $ps, $x)) teardown=(GC.gc(); CUDA.reclaim())
-    else
-        println("  forward")
-        fw(model, x)  #warmup
-        @btime fw($model, $x)
-
-        println("  backward")
-        bw(back)  #warmup
-        @btime bw($back)
-
-        println("  forw and back")
-        fwbw(model, ps, x) # warmup
-        @btime fwbw($model, $ps, $x)
-    end
-end
-
-fw_recur(m, X) = [m(x_t) for x_t in X]
+fw_recur(m, X::AbstractVector{<:AbstractArray}) = [m(x_t) for x_t in X]
+fw_recur(m, X::AbstractArray{<:Number}) = m(X)
 bw_recur(back) = back(1f0)
-fwbw_recur(m, ps, x) = gradient(() -> sum(sum(fw_recur(m, x))), ps)
+fwbw_recur(m, ps, X) = gradient(() -> sum(sum(fw_recur(m, X))), ps)
+
 
 function run_benchmark_recur(model, x; cuda=true, show=false)
     
@@ -107,55 +68,55 @@ function run_benchmark_recur(model, x; cuda=true, show=false)
     (fw=t_fw, bw=t_bw, fwbw=t_fwbw)
 end
 
-function run_benchmark_recur_3d(model, x; cuda=true, show=false)
+# function run_benchmark_recur_3d(model, x; cuda=true, show=false)
     
-    if cuda 
-        model = model |> gpu
-        x = x |> gpu
-    end
+#     if cuda 
+#         model = model |> gpu
+#         x = x |> gpu
+#     end
 
-    ps = Flux.params(model)
-    y, back = pullback(() -> sum(sum(fw_recur(model, x))), ps)
+#     ps = Flux.params(model)
+#     y, back = pullback(() -> sum(sum(fw_recur(model, x))), ps)
 
 
-    t_fw, t_bw, t_fwbw = if cuda
-        CUDA.allowscalar(false)
-        # CUDA.device!(3)
-        fw(model, x); GC.gc(); CUDA.reclaim(); #warmup
-        t_fw = @benchmark CUDA.@sync(fw($model, $x)) teardown=(GC.gc(); CUDA.reclaim())
+#     t_fw, t_bw, t_fwbw = if cuda
+#         CUDA.allowscalar(false)
+#         # CUDA.device!(3)
+#         fw(model, x); GC.gc(); CUDA.reclaim(); #warmup
+#         t_fw = @benchmark CUDA.@sync(fw($model, $x)) teardown=(GC.gc(); CUDA.reclaim())
 
-        bw(back); GC.gc(); CUDA.reclaim(); #warmup
-        t_bw = @benchmark CUDA.@sync(bw($back)) teardown=(GC.gc(); CUDA.reclaim())
+#         bw(back); GC.gc(); CUDA.reclaim(); #warmup
+#         t_bw = @benchmark CUDA.@sync(bw($back)) teardown=(GC.gc(); CUDA.reclaim())
         
-        fwbw(model, ps, x); GC.gc(); CUDA.reclaim(); #warmup
-        t_fwbw = @benchmark CUDA.@sync(fwbw($model, $ps, $x)) teardown=(GC.gc(); CUDA.reclaim())
+#         fwbw(model, ps, x); GC.gc(); CUDA.reclaim(); #warmup
+#         t_fwbw = @benchmark CUDA.@sync(fwbw($model, $ps, $x)) teardown=(GC.gc(); CUDA.reclaim())
         
-        t_fw, t_bw, t_fwbw
-    else
-        Flux.reset!(model)
-        fw(model, x)  #warmup
-        t_fw = @benchmark fw($model, $x)
+#         t_fw, t_bw, t_fwbw
+#     else
+#         Flux.reset!(model)
+#         fw(model, x)  #warmup
+#         t_fw = @benchmark fw($model, $x)
 
-        Flux.reset!(model)
-        bw(back)  #warmup
-        t_bw = @benchmark bw($back)
+#         Flux.reset!(model)
+#         bw(back)  #warmup
+#         t_bw = @benchmark bw($back)
 
-        Flux.reset!(model)
-        fwbw(model, ps, x) # warmup
-        t_fwbw = @benchmark fwbw($model, $ps, $x)
+#         Flux.reset!(model)
+#         fwbw(model, ps, x) # warmup
+#         t_fwbw = @benchmark fwbw($model, $ps, $x)
 
-        Flux.reset!(model)
-        t_fw, t_bw, t_fwbw
-    end
+#         Flux.reset!(model)
+#         t_fw, t_bw, t_fwbw
+#     end
 
-    if show
-        println("\t\tforward: ")
-        @show t_fw
-        println("\t\tbackward: ")
-        @show t_bw
-        println("\t\tforw and back: ")
-        @show t_fwbw
-    end
+#     if show
+#         println("\t\tforward: ")
+#         @show t_fw
+#         println("\t\tbackward: ")
+#         @show t_bw
+#         println("\t\tforw and back: ")
+#         @show t_fwbw
+#     end
 
-    (fw=t_fw, bw=t_bw, fwbw=t_fwbw)
-end
+#     (fw=t_fw, bw=t_bw, fwbw=t_fwbw)
+# end
